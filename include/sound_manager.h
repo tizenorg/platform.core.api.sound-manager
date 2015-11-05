4b86d60d90986475fb039848f2e41515b2c29bb4
inhyeok <i_bc.kim@samsung.com>
2015-11-05 17:04:36 +0900

    Add setting focus auto reacquirement api
    [Version] Release 0.3.24
    [Profile] Common
    [Issue Type] Add feature
    
    Change-Id: Ia76804b6ab55d82d1e2ec322331b3eeea7f86efc
    Signed-off-by: inhyeok <i_bc.kim@samsung.com>

--- include/sound_manager.h
+++ include/sound_manager.h
@@ -624,6 +624,37 @@ int sound_manager_remove_device_for_stream_routing(sound_stream_info_h stream_in
 int sound_manager_apply_stream_routing(sound_stream_info_h stream_info);
 
 /**
+ * @brief Sets auto focus reacquistion property.
+ * @since_tizen 3.0
+ * @param[in]	stream_info			The handle of stream information
+ * @param[in]	reacquisition	The auto focus reacquistion property to set : (@c true = enable, @c false = disable)
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #SOUND_MANAGER_ERROR_NONE Success
+ * @retval #SOUND_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @pre Call sound_manager_create_stream_information() before calling this function.
+ * @see sound_manager_create_stream_information()
+ */
+int sound_manager_set_focus_reacquisition(sound_stream_info_h stream_info, bool reacquisition);
+
+/**
+ * @brief Gets auto focus reacquistion property.
+ * @since_tizen 3.0
+ * @param[in]	stream_info			The handle of stream information
+ * @param[out]	reacquisition	The value of focus reacqusition propery : (@c true = enabled, @c false = disabled)
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #SOUND_MANAGER_ERROR_NONE Success
+ * @retval #SOUND_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @pre Call sound_manager_create_stream_information() before calling this function.
+ * @see sound_manager_create_stream_information()
+ */
+int sound_manager_get_focus_reacquisition(sound_stream_info_h stream_info, bool *reacquisition);
+
+
+/**
  * @brief Acquires the stream focus.
  * @since_tizen 3.0
  * @param[in]	stream_info	The handle of stream information
